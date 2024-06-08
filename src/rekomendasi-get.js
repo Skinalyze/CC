66e6e5318778361f336ef8bf5153f381989b3633
lainyourbae <93993894+lainyourbae@users.noreply.github.com>
2024-06-08 15:45:38 +0700

    Create rekomendasi-get.js

---
+++ src/rekomendasi-get.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const verifyToken = require("./functionToken");
+
+const recommend_get = express();
+recommend_get.use(cors());
+recommend_get.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "skinalyze"
+})
+
+db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }
+    console.log("Connected to the MySQL database.");
+  });  
+
+const dbPromise = db.promise();
+
+// History [GET]
+recommend_get.get('/history', verifyToken, async (req, res) => {
+    const userId = req.user.id_user;
+
+    if (!userId) {
+        return res.status(400).json({ message: "Diperlukan Login untuk mengakses laman ini" });
+    }
+
+    try {
+        const query_1 = "SELECT * FROM rekomendasi WHERE `id_user` = ?";
+        const [rekomendasi] = await dbPromise.query(query_1, [userId]);
+
+        if (rekomendasi.length === 0) {
+            return res.status(404).json({ message: "Not Found: No recomendation found with the given id_user" });
+        }
+        const rekomendasiDetails = rekomendasi.map(row => row.id_rekomendasi);
+
+        return res.status(200).json({ history: rekomendasiDetails });
+
+    } catch (err) {
+        console.error("Error:", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+module.exports = recommend_get;
