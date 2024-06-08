4ed23a6e452099fbe79e1f1d6c237c266f949a6e
lainyourbae <93993894+lainyourbae@users.noreply.github.com>
2024-06-08 15:44:42 +0700

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const mysql = require("mysql");
-const bcrypt = require("bcrypt"); // Jangan lupa untuk mengimpor bcrypt
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 const register = express();
 
 const db = mysql.createConnection({
@@ -10,6 +10,7 @@ const db = mysql.createConnection({
   database: "skinalyze", // sesuaikan dengan nama database Anda
 });
 
+
 db.connect((err) => {
   if (err) {
     console.error("Error connecting to the database:", err);
@@ -18,44 +19,42 @@ db.connect((err) => {
   console.log("Connected to the MySQL database.");
 });
 
-register.post("/register", (req, res) => {
-  const { nama, email, password, gender, age } = req.query;
+const dbPromise = db.promise();
+
+// Register [POST]
+register.post('/register', async (req, res) => {
+  const { nama, email, password, gender, age } = req.body;
 
   // Validasi input
   if (!nama || !email || !password || !gender || !age) {
-    return res.status(400).json({ message: "Terdapat Field yang kosong!" });
+      return res.status(400).json({ message: "Terdapat Field yang kosong!" });
   }
 
-  // Validasi email
-  const query_1 = "SELECT * FROM user WHERE `email` = ?";
-  db.query(query_1, [email], (err, result) => {
-    if (err) {
-      console.error("Error:", err);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+      // Validasi email
+      const query_1 = "SELECT * FROM user WHERE `email` = ?";
+      const [result] = await dbPromise.query(query_1, [email]);
 
-    if (result.length > 0) {
-      return res.status(409).json({ message: "Email sudah terdaftar." });
-    }
-
-    // Enkripsi password
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    // Ubah tipe data gender
-    let genderValue = gender.toLowerCase() === "laki-laki" ? 0 : 1;
-
-    // Memasukan data ke tabel user
-    const query_2 =
-      "INSERT INTO user (`nama`, `email`, `password`, `gender`, `age`) VALUES (?, ?, ?, ?, ?)";
-    const values = [nama, email, hashedPassword, genderValue, age];
-    db.query(query_2, values, (err) => {
-      if (err) {
-        console.error("Error:", err);
-        return res.status(500).json({ message: "Internal Server Error" });
+      if (result.length > 0) {
+          return res.status(409).json({ message: "Email sudah terdaftar." });
       }
 
+      // Enkripsi password
+      const hashedPassword = bcrypt.hashSync(password, 10);
+
+      // Ubah tipe data gender
+      let genderValue = (gender.toLowerCase() === 'laki-laki') ? 0 : 1;
+
+      // Memasukan data ke tabel user
+      const query_2 = "INSERT INTO user (`nama`, `email`, `password`, `gender`, `age`) VALUES (?, ?, ?, ?, ?)";
+      const values = [nama, email, hashedPassword, genderValue, age];
+      await dbPromise.query(query_2, values);
+
       return res.status(201).json({ message: "User berhasil dibuat." });
-    });
-  });
+  } catch (err) {
+      console.error("Error:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
+
 module.exports = register;
