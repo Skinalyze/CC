b4f4a858e4ca1b20513a5c984cf8f785c1de266e
lainyourbae <93993894+lainyourbae@users.noreply.github.com>
2024-06-08 15:46:12 +0700

    Update rekomendasi.js

--- src/rekomendasi.js
+++ src/rekomendasi.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const mysql = require("mysql");
-const recommend = express.Router(); // Menggunakan Router dari express
+const mysql = require("mysql2");
+const recommend = express.Router();
+const verifyToken = require("./functionToken");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -8,6 +9,7 @@ const db = mysql.createConnection({
   password: "", // sesuaikan dengan password database Anda
   database: "skinalyze", // sesuaikan dengan nama database Anda
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -17,68 +19,71 @@ db.connect((err) => {
   console.log("Connected to the MySQL database.");
 });
 
+const dbPromise = db.promise();
+
 // Middleware untuk parsing JSON request body
 recommend.use(express.json());
 
-// Route untuk mendapatkan produk berdasarkan kriteria
-recommend.get("/products", (req, res) => {
-  const { sensitif, id_tipe_skin_type, id_skin_problem } = req.query;
-  console.log(sensitif, id_tipe_skin_type, id_skin_problem);
+// Recommendations [GET]
+recommend.get('/recommendation', verifyToken, async (req, res) => {
+  const { id_tipe_skin_type, id_skin_problem } = req.query;
+  const userId = req.user.id_user;
 
-  let query = `
-    SELECT skin_care.*
-    FROM skin_care
-    LEFT JOIN skin_care_type ON skin_care.id_skin_care = skin_care_type.id_skin_care
-    LEFT JOIN skin_care_problem ON skin_care.id_skin_care = skin_care_problem.id_skin_care
-    WHERE 1=1
+  if (!userId) {
+      return res.status(400).json({ message: "Diperlukan Login untuk mengakses laman ini" });
+  }
+
+  const sensitif = req.user.sensitif;
+
+  let queryText = `
+      SELECT skin_care.*
+      FROM skin_care
+      LEFT JOIN skin_care_type ON skin_care.id_skin_care = skin_care_type.id_skin_care
+      LEFT JOIN skin_care_problem ON skin_care.id_skin_care = skin_care_problem.id_skin_care
+      WHERE 1=1
   `;
   const queryParams = [];
 
   if (sensitif) {
-    query += " AND skin_care.sensitif = ?";
-    queryParams.push(sensitif);
+      queryText += " AND skin_care.sensitif = ?";
+      queryParams.push(sensitif);
   }
 
   if (id_tipe_skin_type) {
-    query += " AND skin_care_type.id_tipe_skin_type = ?";
-    queryParams.push(id_tipe_skin_type);
+      queryText += " AND skin_care_type.id_tipe_skin_type = ?";
+      queryParams.push(id_tipe_skin_type);
   }
 
   if (id_skin_problem) {
-    const ids = id_skin_problem.split(",").map((id) => id.trim());
-    query += ` AND skin_care_problem.id_skin_problem IN (${ids
-      .map(() => "?")
-      .join(",")})`;
-    queryParams.push(...ids);
+      const ids = id_skin_problem.split(",").map((id) => id.trim());
+      queryText += ` AND skin_care_problem.id_skin_problem IN (${ids.map(() => "?").join(",")})`;
+      queryParams.push(...ids);
   }
 
   // Mengacak hasil di query
-  query += " ORDER BY RAND()";
+  queryText += " ORDER BY RAND()";
 
-  db.query(query, queryParams, (err, results) => {
-    if (err) {
-      console.error("Error executing query:", err);
-      res.status(500).send("Server error");
-      return;
-    }
-
-    // Memproses hasil untuk mendapatkan 3 item acak untuk setiap jenis id_product_type
+  try {
+    const results = await dbPromise.query(queryText, queryParams);
     const groupedResults = {};
     results.forEach((result) => {
-      const type = result.id_product_type;
-      if (!groupedResults[type]) {
-        groupedResults[type] = [];
-      }
-      if (groupedResults[type].length < 3) {
-        groupedResults[type].push(result);
-      }
+        const type = result.id_product_type;
+        if (!groupedResults[type]) {
+            groupedResults[type] = [];
+        }
+        if (groupedResults[type].length < 3) {
+            groupedResults[type].push(result);
+        }
     });
 
     // Menggabungkan hasil dari setiap grup menjadi satu array
     const finalResults = Object.values(groupedResults).flat();
 
     res.json(finalResults);
-  });
+  } catch (err) {
+      console.error("Error executing query:", err);
+      res.status(500).send("Server error");
+  }
 });
 
 module.exports = recommend;
