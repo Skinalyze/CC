54bcf6b2c6600a2320c39c9ff514c0363943d552
lainyourbae <93993894+lainyourbae@users.noreply.github.com>
2024-06-20 08:58:36 +0700

    Update skintype-update.js

--- src/skintype-update.js
+++ src/skintype-update.js
@@ -1,42 +1,35 @@
 const express = require("express");
-const mysql = require("mysql");
 const update = express();
+const verify_token = require("./functionToken");
+const dbPromise = require("./db");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", // sesuaikan dengan username database Anda
-  password: "", // sesuaikan dengan password database Anda
-  database: "skinalyze", // sesuaikan dengan nama database Anda
-});
+const cors = require("cors");
+update.use(cors());
+update.use(express.json());
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-    return;
-  }
-  console.log("Connected to the MySQL database.");
-});
+update.put('/skintype', verify_token, async (req, res) => {
+  const { skintypes, sensitif } = req.body;
 
-update.put("/skintype", (req, res) => {
-  const { id_user, skintypes, sensitif } = req.query;
+  const user_id = req.user.id_user;
 
-  // Validasi input
-  if (!id_user || !skintypes || !sensitif) {
-    return res.status(400).json({ message: "Terdapat Field yang kosong!" });
+  if (!user_id) {
+      return res.status(400).json({ message: "Diperlukan Login untuk mengakses laman ini" });
   }
 
-  // Melakukan update data ke tabel user
-  const sql =
-    "UPDATE user SET `skintypes` = ?, `sensitif` = ? WHERE `id_user` = ?";
-  const values = [skintypes, sensitif, id_user];
-  db.query(sql, values, (err) => {
-    if (err) {
+  if (!skintypes || !sensitif) {
+      return res.status(400).json({ message: "Terdapat Field yang kosong!" });
+  }
+
+  try {
+      const query_1 = "UPDATE user SET `skintypes` = ?, `sensitif` = ? WHERE `id_user` = ?";
+      const values = [skintypes, sensitif, user_id];
+      await dbPromise.query(query_1, values);
+
+      return res.status(200).json({ message: "Skin type berhasil diperbarui!" });
+  } catch (err) {
       console.error("Error:", err);
       return res.status(500).json({ message: "Internal Server Error" });
-    }
-
-    return res.status(201).json({ message: "Skin type updated successfully." });
-  });
+  }
 });
 
 module.exports = update;
