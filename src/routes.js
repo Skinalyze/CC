450e1eb5019b76cb9cf91f175aa9eb7a3f2bbc4d
lainyourbae <93993894+lainyourbae@users.noreply.github.com>
2024-06-20 08:57:27 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,29 @@
 const express = require("express");
-const recommend = require("./rekomendasi");
+const recommend_post = require("./rekomendasi-post");
+const recommend_get = require("./rekomendasi-get");
+const recommend_get_detail = require("./rekomendasi-get-detail");
 const register = require("./register");
 const hapus = require("./rekomendasi-delete");
 const login = require("./login");
+const logout = require("./logout");
+const search = require("./search");
+const detail = require("./search-detail");
 const update = require("./skintype-update");
+const profil = require("./profil");
+const token = require("./token");
 const router = express.Router();
 
-// Rute untuk produk rekomendasi
-router.use("/", recommend);
 router.use("/", hapus);
 router.use("/", register);
 router.use("/", login);
+router.use("/", logout);
 router.use("/", update);
+router.use("/", search);
+router.use("/", detail);
+router.use("/", recommend_get);
+router.use("/", recommend_post);
+router.use("/", recommend_get_detail);
+router.use("/", profil);
+router.use("/", token);
 
 module.exports = router;
